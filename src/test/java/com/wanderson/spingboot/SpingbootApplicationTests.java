f74176282faf864e10dd9945ffce740c7fdf10fa
Wanderson Santos <wsantos.rj@outlook.com>
2022-03-30 20:29:16 -0300

    Created Project

---
+++ src/test/java/com/wanderson/spingboot/SpingbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanderson.spingboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
